1113f9faa8d4ed30626e2af2a7d2c3a275553865
Rahul Dey <rahuldey9623@gmail.com>
2025-09-30 07:10:01 +0000

    Refector product card into own component

---
+++ src/components/product-card.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { IProduct } from '$lib/types';
+	import { addItemToCart } from '$lib/cart-store.svelte';
+
+	type IProps = {
+		product: IProduct;
+	};
+
+	const { product }: IProps = $props();
+</script>
+
+<section>
+	<div class="overflow-hidden rounded-xl bg-white shadow-lg relative">
+		<img src={product.thumbnail} alt={product.title} class="h-48 w-full object-cover" />
+        <span class="text-center bg-slate-100 px-2 py-1 rounded-full absolute top-2.5 right-2.5">{product.rating.toFixed(1)}⭐</span>
+		<div class="p-4">
+			<p class="mb-2 truncate overflow-hidden text-lg font-medium text-gray-800">
+				{product.title}
+			</p>
+			<div class="flex items-center justify-between">
+				<p class="text-xl font-bold">${product.price}</p>
+				<button
+					class="rounded-full bg-sky-600 px-4 py-2 text-white transition-colors duration-300 hover:bg-sky-700"
+					onclick={() => addItemToCart(product)}
+				>
+					Add to cart
+				</button>
+			</div>
+		</div>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
 	import ShoppingCart from 'phosphor-svelte/lib/ShoppingCart';
 	import CartModal from '../components/cart-modal.svelte';
-	import {
-		addItemToCart,
-		isCartModalOpen,
-		getCartStats,
-		toggleCartModal
-	} from '$lib/cart-store.svelte';
+	import { isCartModalOpen, getCartStats, toggleCartModal } from '$lib/cart-store.svelte';
+	import ProductCard from '../components/product-card.svelte';
 
 	const { data } = $props();
 </script>
@@ -35,22 +31,6 @@
 
 <div class="grid grid-cols-1 gap-6 bg-gray-100 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 	{#each data.products as product (product.id)}
-		<div class="overflow-hidden rounded-xl bg-white shadow-lg">
-			<img src={product.thumbnail} alt={product.title} class="h-48 w-full object-cover" />
-			<div class="p-4">
-				<p class="mb-2 truncate overflow-hidden text-lg font-medium text-gray-800">
-					{product.title}
-				</p>
-				<div class="flex items-center justify-between">
-					<p class="text-xl font-bold">${product.price}</p>
-					<button
-						class="rounded-full bg-sky-600 px-4 py-2 text-white transition-colors duration-300 hover:bg-sky-700"
-						onclick={() => addItemToCart(product)}
-					>
-						Add to cart
-					</button>
-				</div>
-			</div>
-		</div>
+		<ProductCard {product} />
 	{/each}
 </div>
